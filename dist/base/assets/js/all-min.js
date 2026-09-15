d975d7469ed0d2329531c32e6eeb87358d34d303
Igor Gavelyuk <gavelyukworking@gmail.com>
2026-09-15 13:56:24 +0200

    rebuild

--- dist/base/assets/js/all-min.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(n){document.getElementById("nav-button").onclick=function(){document.getElementById("nav-menu").classList.toggle("shown");const n=document.getElementById("menu-close");"MENU"==n.innerHTML?n.innerHTML="CLOSE":n.innerHTML="MENU"}}));
-//# sourceMappingURL=index.min.js.map
-
-//# sourceMappingURL=all-min.js.map
